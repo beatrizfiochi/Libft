4bc14c460245f0bc45bc7855eee5106ef3c526ad
Beatriz Fiochi Soares da Silva <129054983+beatrizfiochi@users.noreply.github.com>
2024-04-14 17:33:05 +0100

    Add files via upload

---
+++ libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bfiochi- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/14 14:26:04 by bfiochi-          #+#    #+#             */
+/*   Updated: 2024/04/14 16:56:58 by bfiochi-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdio.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+
+#endif
