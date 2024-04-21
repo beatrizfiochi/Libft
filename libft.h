483a2d2a2452f4ede6c8ba57f7399b7340502192
Beatriz Fiochi Soares Da Silva <bfiochi-@c1r8s3.42porto.com>
2024-04-21 17:46:44 +0100

    Add ft_toupper.c and updating libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bfiochi- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 14:26:04 by bfiochi-          #+#    #+#             */
-/*   Updated: 2024/04/21 17:01:52 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2024/04/21 17:45:08 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 
 # include <stdio.h>
 # include <string.h>
+# include <ctype.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int		ft_toupper(int c);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 void	ft_bzero(void *s, size_t n);
