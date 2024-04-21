03f4b26f45df8b0bd878b71d98225390fc585f07
Beatriz Fiochi Soares Da Silva <bfiochi-@c1r8s3.42porto.com>
2024-04-21 19:41:08 +0100

    Add ft_strncmp.c and updating libft,h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bfiochi- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 14:26:04 by bfiochi-          #+#    #+#             */
-/*   Updated: 2024/04/21 18:52:12 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2024/04/21 19:40:20 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
