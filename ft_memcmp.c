d04b381a4ec593369ec9a8e5a99ebd6b3dfd8493
Beatriz Fiochi Soares Da Silva <bfiochi-@c1r13s6.42porto.com>
2024-04-22 17:29:23 +0100

    Add ft_memcmp.c

---
+++ ft_memcmp.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bfiochi- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 17:08:53 by bfiochi-          #+#    #+#             */
+/*   Updated: 2024/04/22 17:27:59 by bfiochi-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*str1;
+	unsigned char	*str2;
+	size_t			i;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n && (str1[i] != '\0' || str2[i] != '\0'))
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
+}
+
+/*int	main(void)
+{
+	printf("%d\n", memcmp("Beatriza", "Beatriz", 0));
+	printf("%d\n", ft_memcmp("Beatriza", "Beatriz", 0));
+}*/
