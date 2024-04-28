cebd3d2ea947fd43372b997ed73127beca9568b9
Beatriz Fiochi Soares Da Silva <bfiochi-@c1r8s5.42porto.com>
2024-04-28 15:26:14 +0100

    Add ft_calloc.c and updating libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bfiochi- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 14:26:04 by bfiochi-          #+#    #+#             */
-/*   Updated: 2024/04/28 12:51:45 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2024/04/28 15:25:27 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <stdio.h>
 # include <string.h>
+# include <stdlib.h>
 # include <ctype.h>
 
 int		ft_isalpha(int c);
@@ -38,5 +39,6 @@ void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
 
 #endif
