3fc6685a2007b2f17721300359270b43df89ab5a
Beatriz Fiochi Soares Da Silva <bfiochi-@c1r8s1.42porto.com>
2024-05-01 09:45:25 +0100

    Updating ft_putnbr_fd.c and libft.h

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: bfiochi- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 13:58:53 by bfiochi-          #+#    #+#             */
-/*   Updated: 2024/04/29 14:26:33 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2024/05/01 09:43:32 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,27 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
+	long	nb;
+
 	if (fd > 0)
 	{
-
+		nb = n;
+		if (nb < 0)
+		{
+			ft_putchar_fd('-', fd);
+			nb = nb * (-1);
+		}
+		if (nb > 9)
+		{
+			ft_putnbr_fd(nb / 10, fd);
+			ft_putchar_fd((nb % 10) + '0', fd);
+		}
+		else
+			ft_putchar_fd(nb + '0', fd);
 	}
 }
 
-int	main(void)
+/*int	main(void)
 {
-	ft_putnbr_fd('5', 1);
-	ft_putnbr_fd('\n', 1);
-}
+	ft_putnbr_fd(-2147483648, 1);
+}*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bfiochi- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 14:26:04 by bfiochi-          #+#    #+#             */
-/*   Updated: 2024/04/28 15:52:52 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2024/05/01 09:44:33 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strdup(const char *s);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
@@ -45,5 +46,6 @@ void	*ft_calloc(size_t nmemb, size_t size);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
