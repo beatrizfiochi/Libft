64f083060bf4ff50ae318eef65a22383ce43ef99
Beatriz Fiochi Soares Da Silva <bfiochi-@c2r5s1.42porto.com>
2024-05-07 14:49:21 +0100

    Fixing and updating ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: bfiochi- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 17:08:53 by bfiochi-          #+#    #+#             */
-/*   Updated: 2024/04/22 17:27:59 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2024/05/07 14:47:40 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	i = 0;
-	while (i < n && (str1[i] != '\0' || str2[i] != '\0'))
+	while (i < n)
 	{
 		if (str1[i] != str2[i])
 			return (str1[i] - str2[i]);
