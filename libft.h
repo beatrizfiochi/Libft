41167345a2df331fe4a5c6fcc78ba58ce75e80b7
Beatriz Fiochi Soares Da Silva <bfiochi-@c1r10s3.42porto.com>
2024-05-20 16:54:55 +0100

    Add ft_lstadd_back.c and updating libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bfiochi- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 14:26:04 by bfiochi-          #+#    #+#             */
-/*   Updated: 2024/05/20 16:35:21 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2024/05/20 16:54:19 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,7 @@ typedef struct s_list
 t_list	*ft_lstnew(void *content);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 
 #endif
